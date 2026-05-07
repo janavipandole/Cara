7b9bdb24101ee803516c934c68880a2a3d5da263
Akshita <akpandey2307@gmail.com>
2026-05-07 17:46:34 +0530

    feat: add Home page composing all sections

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Expertise from '../components/Expertise'
+import CaseStudies from '../components/CaseStudies'
+import CTA from '../components/CTA'
+import Footer from '../components/Footer'
+
+function Home() {
+    return(
+        <>
+        <Navbar />
+        <Hero />
+        <Expertise />
+        <CaseStudies />
+        <CTA />
+        <Footer />
+        </>
+    )
+}
+export default Home
