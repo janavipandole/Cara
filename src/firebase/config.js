38a239605926e3550ae7a4e8a27ef801f122aa0b
Akshita <akpandey2307@gmail.com>
2026-05-07 17:45:44 +0530

    feat: add Firebase app initialization and auth export

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAeixdgwXM0AKNanKhEaRcQKzKEl2vgr5E",
+  authDomain: "precision-58983.firebaseapp.com",
+  projectId: "precision-58983",
+  storageBucket: "precision-58983.firebasestorage.app",
+  messagingSenderId: "843334805854",
+  appId: "1:843334805854:web:2b31ec97d9376eb74947cf",
+  measurementId: "G-1SZWCS51B7"
+}
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
