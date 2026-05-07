1883342b8a9caed396ac9b721d4da59d4c2bd757
Akshita <akpandey2307@gmail.com>
2026-05-07 22:23:55 +0530

    cleanup: remove unused import from firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, connectAuthEmulator } from 'firebase/auth'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyAeixdgwXM0AKNanKhEaRcQKzKEl2vgr5E",
