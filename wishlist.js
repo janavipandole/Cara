29dc50b8fdc3756bae81713d2176b3a5f182553c
Ananya-jainn <ananyaajain08@gmail.com>
2026-05-16 01:36:29 +0530

    wishlist changes

--- wishlist.js
+++ wishlist.js
@@ -1,33 +1,32 @@
+const container = document.getElementById("wishlist-container");
+// Changed const to let so we can easily update the array
+let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
 function renderWishlist() {
-   console.log("wishlist updated")
+  if (!container) return;
+
+  container.innerHTML = "";
+  wishlist.forEach((item) => {
+    container.innerHTML += `
+         <div class="card">
+            <img src="${item.image}">
+            <h2>${item.name}</h2>
+            <p>${item.price}</p>
+            <button onclick="removeItem('${item.id}')">
+               <i class="ri-delete-bin-line"></i>
+            </button>
+         </div>
+      `;
+  });
 }
 
-function removeItem(wishlist) {
+function removeItem(id) {
+  // 1. Filter out the item
+  wishlist = wishlist.filter((item) => item.id !== id);
 
-   wishlist = wishlist.filter((item) => {
-      return item.id !== id
-   })
+  
+  localStorage.setItem("wishlist", JSON.stringify(wishlist));
 
-   renderWishlist()
+  
+  renderWishlist();
 }
-function add_prods(){
-    button.addEventListener("click",()=>{
-    wishlist.push(product);
-    })
-}
-
-function remove_prods(){
-    button.addEventListener("click",()=>{
-    wishlist.push(product);
-    })
-}
-
-
-function display_items(){
-    for(let prods in wishlist){
-        console.log(wishlist)
-    }
-}
-
-
-
