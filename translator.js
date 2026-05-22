73781b421522469eef3f5d0ee9a14303d4372c92
RupDebnath <kamaldebnath415@gmail.com>
2026-05-22 06:13:27 +0530

    language translator

---
+++ translator.js
@@ -0,0 +1,9 @@
+window.gtranslateSettings = {
+  default_language: "en",
+  languages: ["en", "hi", "bn", "fr", "de", "es","ar","ja","zh-CN","ru",],
+  wrapper_selector: ".gtranslate_wrapper",
+  switcher_horizontal_position: "left",
+  switcher_vertical_position: "bottom",
+  native_language_names: true,
+  detect_browser_language: true
+};
