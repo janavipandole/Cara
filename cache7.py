643d5a3aefd7e96407a2acf20b985cc4ee3a950e
VishnuVardhanCodes <vishnumaxpolla32@gmail.com>
2026-05-26 00:34:08 +0530

    added the new features to the navbar section like hover animation

---
+++ cache7.py
@@ -0,0 +1,11 @@
+﻿import os, glob
+
+for root in ['.', 'Cara-main']:
+    for f_name in glob.glob(os.path.join(root, '*.html')):
+        with open(f_name, 'r', encoding='utf-8') as f:
+            content = f.read()
+            
+        content = content.replace('style.css?v=6', 'style.css?v=7')
+        
+        with open(f_name, 'w', encoding='utf-8') as f:
+            f.write(content)
