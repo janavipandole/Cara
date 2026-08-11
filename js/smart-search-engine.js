53f089f1449bb899e820f036c39f17df34e2aa14
tmdeveloper007 <tmdeveloper007@users.noreply.github.com>
2026-08-11 09:20:20 +0530

    chore: added getSmartSearchEngineStatusHelper77 helper to js/smart-search-engine.js

--- js/smart-search-engine.js
+++ js/smart-search-engine.js
@@ -117,3 +117,8 @@ if (typeof module !== 'undefined' && module.exports) {
 } else {
   window.SmartSearchEngine = SmartSearchEngine;
 }
+
+
+export function getSmartSearchEngineStatusHelper77() {
+  return { status: "ok", fn: "getSmartSearchEngineStatusHelper77" };
+}
